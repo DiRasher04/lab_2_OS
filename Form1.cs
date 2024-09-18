5e8f16a8ad2a3b9bcbc379965cfbd0b13f61cd2c
veegr <veegr@DIRASHER>
2024-09-18 10:08:29 +0300

    Добавьте файлы проекта.

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace lab_2_OS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK) 
+            { 
+                ofd.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                label1.Text = ofd.FileName;
+            }
+            Process process = new Process();
+            process.StartInfo = new ProcessStartInfo(ofd.FileName);
+            process.Start();
+        }
+    }
+}
